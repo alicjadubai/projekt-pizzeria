fa8d55fecbe28e07ad475ee07b1c5e43d8c44e6b
Alicja Jagielska <alicja_dubai@wp.pl>
2022-01-26 18:23:10 +0100

    8.4 PrepareCartProduct puste

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -264,12 +264,40 @@
         amount: thisProduct.amountWidget.value,
         priceSingle: thisProduct.priceSingle,
         price: thisProduct.price,
-        params: {},
+        params: thisProduct.prepareCartProductParams(),
       };
       return prepareCartProduct;
     }
     prepareCartProductParams() {
       const thisProduct = this;
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      const params = {};
+
+      // for very category (param)
+      for (let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+
+        // create category param in params const eg. params = { ingredients: { name: 'Ingredients', options: {}}}
+        params[paramId] = {
+          label: param.label,
+          options: {},
+        };
+
+        // for every option in this category
+        for (let optionId in param.options) {
+          const option = param.options[optionId];
+          console.log("option", option);
+          const optionSelected =
+            formData[paramId] && formData[paramId].includes(optionId);
+
+          if (optionSelected) {
+            // option is selected!
+            params[paramId].options.option = optionSelected;
+          }
+        }
+      }
+
+      return params;
     }
   }
 
